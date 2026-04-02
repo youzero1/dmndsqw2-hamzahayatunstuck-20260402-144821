8681ba2eefb8754cea5363c2bc255189993b6e4e
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-04-02 14:50:01 +0500

    Initial commit from Flare Codegen (API)

---
+++ todo-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import TodoApp from '@/components/TodoApp'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-100 py-12 px-4">
+      <TodoApp />
+    </main>
+  )
+}
